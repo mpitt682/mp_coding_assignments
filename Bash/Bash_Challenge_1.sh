902a49c86bf2aae321a56f9ec9e3549a27415cbe
Maggie Pitt <pittmf@gmail.com>
2025-12-01 17:30:48 -0600

    Update Bash_Challenge_1.sh

--- Bash/Bash_Challenge_1.sh
+++ Bash/Bash_Challenge_1.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# store output of df command to text file
 df > df_output.txt 
 
 declare -u header="The is the df command output in green and italic formatting"
@@ -8,5 +9,5 @@ echo $header
 #declare -i lines=$(df | wc -l)
 #echo $lines
 
-
+# output df with green and italic formatting
 echo -e "\033[1;34;40mLINE:\n\033[0m\033[3;32;40m$(cat df_output.txt)\033[0m"
